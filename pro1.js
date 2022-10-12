250a28175c72ec32ecc6f4068d1f05d638b69431
swetamaurya <mauryasweta062@gmail.com>
2022-10-12 18:53:47 +0530

    done

---
+++ pro1.js
@@ -0,0 +1,19 @@
+//username and password stored in the database while registering
+let database_user="abs@gmail.com";
+  let database_pass="asbu156";
+
+
+//for login
+let username="abs@gmail.com";
+  let password="asbu156";
+
+if (database_user==username){
+if (database_pass==password){
+  console.log("Login Successful")
+} else{
+  console.log("incorrect password");
+}
+
+}else{
+  console.log("Wrong credentials")
+}
---
+++ pro3.js
@@ -0,0 +1,8 @@
+let num=10;
+if (num%5==0){
+  console.log("Multiple of 5");
+} else if(num %2==0){
+  console.log("Multiple of 2");
+}else{
+  console.log("NA");
+}
